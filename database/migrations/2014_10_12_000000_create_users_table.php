d09d73c7323ff1392c2d08fb4c5675c69550f7a7
Psiu <psiu7217@gmail.com>
2019-06-28 17:55:22 +0300

    Добавил ругистрацию и бутстрап

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 class CreateUsersTable extends Migration
 {
@@ -13,15 +14,39 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        Schema::create('user_roles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->timestamps();
+        });
+
+        DB::table('user_roles')->insert(
+            [
+                ['title' => 'Администратор', 'slug' => 'admin'],
+                ['title' => 'Пользователь', 'slug' => 'user'],
+                ['title' => 'Гость', 'slug' => 'guest']
+            ]
+        );
+
+
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('login')->nullable();
+            $table->string('name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('img')->nullable();
+            $table->bigInteger('role')->unsigned()->default(3);
             $table->rememberToken();
             $table->timestamps();
         });
+
+        Schema::table('users', function ($table) {
+            $table->foreign('role')->references('id')->on('user_roles');
+        });
+
     }
 
     /**
@@ -32,5 +57,6 @@ class CreateUsersTable extends Migration
     public function down()
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('user_roles');
     }
 }
