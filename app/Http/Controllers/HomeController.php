4c14156b5fb90291fdea94cfebd45130dbae00c5
Psiu <psiu7217@gmail.com>
2019-06-28 18:04:44 +0300

    Add debugbar

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -13,6 +14,9 @@ class HomeController extends Controller
      */
     public function __construct()
     {
+        $user = Auth::user();
+        \Debugbar::info($user);
+
         $this->middleware('auth');
     }
 
@@ -23,6 +27,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+
         return view('home');
     }
 }
